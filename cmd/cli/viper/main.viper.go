270318bf7a44b6375914e79301e92e7aa83abb7f
Châu Thịnh <chauthinh00710@gmail.com>
2024-07-08 08:42:45 +0700

    gin and viper

--- cmd/cli/viper/main.viper.go
+++ cmd/cli/viper/main.viper.go
@@ -6,6 +6,18 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Server struct {
+		Port int `mapstructure:"port"`
+	} `mapstructure:"server"`
+
+	Databases []struct {
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Host     string `mapstructure:"host"`
+	} `mapstructure:"databases"`
+}
+
 func main() {
 	viper := viper.New()
 	viper.AddConfigPath("./config/")
@@ -14,9 +26,22 @@ func main() {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Failed to read config %w \n", err))
+		panic(fmt.Errorf("failed to read config: %w ", err))
 	}
 	//read server configuration
 	fmt.Println("Server port::", viper.GetInt("server.port"))
-	fmt.Println("Server port::", viper.GetString("security.jwt.key"))
+	fmt.Println("Security key::", viper.GetString("security.jwt.key"))
+
+	//configure structure
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Printf("unable to decode configuration %v", err)
+
+	}
+	fmt.Println("Config port::", config.Server.Port)
+	for _, db := range config.Databases {
+		fmt.Printf("database User: %s, password: %s, host: %s", db.User, db.Password, db.Host)
+		fmt.Print(1)
+	}
 }
